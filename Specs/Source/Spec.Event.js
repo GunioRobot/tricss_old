ea042d2ca545df2c3290f11f919e22e89b4decd9
Chris Schneider <chris@chris-bk.de>
2008-08-25 15:19:42 +0200

    added testing for \!when

--- Specs/Source/Spec.Event.js
+++ Specs/Source/Spec.Event.js
@@ -6,8 +6,14 @@ describe('Events', {
 			i++;
 			element.removeTricssEvent(['hover', 'focus'], 'enter', fn);
 		};
+		
+		function fn2(){
+			i++;
+			element.removeTricssEvent('hover', fn2);
+		};
 				
 		element.addTricssEvent(['hover', 'focus'], 'enter', fn);
+		element.addTricssEvent('hover', fn2);
 		element.addTricssEvent(['hover', 'focus'], 'enter', fn);
 		
 		element.fireEvent('mouseenter');
@@ -15,6 +21,6 @@ describe('Events', {
 		element.fireEvent('blur');
 		element.fireEvent('focus');
 		
-		value_of(i).should_be(1);
+		value_of(i).should_be(2);
 	}
 });
